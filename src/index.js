878b1d100685e661f03ae5919d8e24806dab9b76
Jane Lee <jane.lee.19@dartmouth.edu>
2017-04-25 22:42:57 -0400

    forgot test id

--- src/index.js
+++ src/index.js
@@ -38,6 +38,8 @@ const Nav = (props) => {
       <ul>
         <li><NavLink to="/" exact>Home</NavLink></li>
         <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
       </ul>
     </nav>
   );
